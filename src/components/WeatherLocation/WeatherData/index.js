865a09ac86de3d12ca984e3060036196d447916e
Marco <symarcs@msn.com>
2019-08-19 01:23:43 -0500

    Componentes Clase-Funcionales

--- src/components/WeatherLocation/WeatherData/index.js
+++ src/components/WeatherLocation/WeatherData/index.js
@@ -1,23 +1,26 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import WeatherExtraInfo from './WeatherExtraInfo';
 import WeatherTemperature from './WeatherTemperature';
-import {
-    CLOUD,
-    CLOUDY,
-    SUN,
-    RAIN,
-    SNOW,
-    WINDY,
-} from './../../../constants/weathers';
+import './styles.css';
 
-const WeatherData = () => (
-    <div>
-        <WeatherTemperature
-            temperature={20}
-            weatherState={RAIN}
-        />
-        <WeatherExtraInfo humidity={80} wind={"10 m/s"}></WeatherExtraInfo>
-    </div>
+const WeatherData = ({data: {temperature, weatherState, humidity, wind}}) => (
+        <div className="weatherDataCont">
+            <WeatherTemperature
+                temperature={temperature}
+                weatherState={weatherState}
+            />
+            <WeatherExtraInfo humidity={humidity} wind={wind}></WeatherExtraInfo>
+        </div>
 );
+
+WeatherData.propTypes = {
+    data: PropTypes.shape({
+        temperature: PropTypes.number.isRequired,
+        weatherState:PropTypes.string.isRequired,
+        humidity:PropTypes.number.isRequired,
+        wind:PropTypes.string.isRequired,
+    }),
+}
 
 export default WeatherData;
--- src/components/WeatherLocation/index.js
+++ src/components/WeatherLocation/index.js
@@ -1,12 +1,36 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Location from './Location';
 import WeatherData from './WeatherData';
+import {
+    SUN
+} from './../../constants/weathers';
+import './styles.css';
 
-const WeatherLocation = () => (
-    <div>
-        <Location city={"CDMXX"}></Location>
-        <WeatherData></WeatherData>
+const data = {
+    temperature: 6,
+    weatherState: SUN,
+    humidity: 10,
+    wind: '10 m/s'
+}
+
+//COMPONENTE FUNCIONAL
+/*const WeatherLocation = () => (
+    <div className="weatherLocationCont">
+        <Location city={"CDMX"}></Location>
+        <WeatherData data={data}></WeatherData>
     </div>
-);
+);*/
+
+//COMPONENTE CLASE
+class WeatherLocation extends Component {
+    render() {
+        return (
+            <div className="weatherLocationCont">
+                <Location city={"CDMX"}></Location>
+                <WeatherData data={data}></WeatherData>
+            </div>
+        );
+    }
+};
 
 export default WeatherLocation;
